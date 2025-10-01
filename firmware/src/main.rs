dbce5eb506e72cb87bf530dd8b034f4b6d18ad26
Husamettin ARABACI <harabaci88@gmail.com>
2025-10-01 15:01:29 +0300

    feat(setup): Add first setup firmware (#10)
    Initialized first firmware setup

---
+++ firmware/src/main.rs
@@ -0,0 +1,36 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_rp::bind_interrupts;
+use embassy_rp::gpio;
+use embassy_rp::peripherals::USB;
+use embassy_rp::usb::Driver;
+use embassy_rp::usb::InterruptHandler;
+use embassy_time::Timer;
+use gpio::{Level, Output};
+use {defmt_rtt as _, panic_probe as _};
+
+mod usb;
+
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    let driver = Driver::new(p.USB, Irqs);
+    let usb::UsbMidi { device, midi } = usb::init(driver);
+    spawner.spawn(usb::dev_task(device)).unwrap();
+    spawner.spawn(usb::usb_io_task(midi)).unwrap();
+
+    let mut led = Output::new(p.PIN_25, Level::Low);
+    loop {
+        led.set_high();
+        Timer::after_secs(1).await;
+        led.set_low();
+        Timer::after_secs(1).await;
+    }
+}
